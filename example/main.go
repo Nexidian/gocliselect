e52d53babf22242cfe1c0f7c3449bc7823b50b27
Luke <obsidian1710@gmail.com>
2022-01-30 22:48:39 +0000

    Adjust package name

--- example/main.go
+++ example/main.go
@@ -1,9 +1,9 @@
 package main
 
-import "go-cli-select"
+import "github.com/Nexidian/gocliselect"
 
 func main() {
-	menu := go_cli_select.NewMenu("", "Select an option")
+	menu := gocliselect.NewMenu("", "Select an option")
 
 	menu.AddItem("Option 1", "option1")
 	menu.AddItem("Option 2", "option2")
--- go-cli-select.go
+++ go-cli-select.go
@@ -1,4 +1,4 @@
-package go_cli_select
+package gocliselect
 
 import (
 	"fmt"
